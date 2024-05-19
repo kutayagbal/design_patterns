c74b1c573d4a3993f48158214e3a9c4b8451f51a
Kutay Agbal <kutayagbal@gmail.com>
2024-05-19 04:04:59 +0200

    factory method principles

--- src/main/java/gof/creational/factory_method/FactoryMethodTest.java
+++ src/main/java/gof/creational/factory_method/FactoryMethodTest.java
@@ -6,6 +6,21 @@ import gof.creational.factory_method.store.NYStylePizzaStore;
 import gof.creational.factory_method.store.PizzaStore;
 
 public class FactoryMethodTest {
+    /*
+     * FACTORY METHOD: Defines an interface for creating an object, but lets
+     * subclasses decide which class to instantiate. Factory method lets a class to
+     * defer instantiation to subclasses.
+     * 
+     * Factory method handles object creation and encapsulates it in subclass. This
+     * decouples the client code in the superclass from the object creation code in
+     * the subclass.
+     * 
+     * Identify the aspects that vary and seperate them from what stays the same.
+     * 
+     * Dependency Inversion: Depend upon abstractions, do not depend upon concrete
+     * classes.
+     * 
+     */
     public static void main(String[] args) {
         PizzaStore nyStore = new NYStylePizzaStore();
         PizzaStore chicagoStore = new ChicagoStylePizzaStore();
